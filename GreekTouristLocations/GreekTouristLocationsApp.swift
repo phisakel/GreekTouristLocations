68109930c67818b0840f75500aa0a6bfdd8668a3
Filippos Sakellaropoulos <phisakel@gmail.com>
2022-11-13 22:49:16 +0200

    Initial Commit

---
+++ GreekTouristLocations/GreekTouristLocationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreekTouristLocationsApp.swift
+//  GreekTouristLocations
+//
+//  Created by ffeli on 13/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct GreekTouristLocationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
